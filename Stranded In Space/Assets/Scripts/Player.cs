156aa5816986edd4e5344e48600c3198116452ac
andrewrudasics <43798507+andrewrudasics@users.noreply.github.com>
2020-02-05 12:17:46 -0800

    added initial project

---
+++ Stranded In Space/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private GameObject stuckTo;
+    public float stickMultiplier = 100;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stuckTo = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.GetComponent<Rigidbody2D>().AddForce(stickMultiplier * (stuckTo.transform.position - transform.position).normalized);
+    }
+
+    public void StickTo(GameObject other)
+    {
+        stuckTo = other;
+    }
+
+    public void releaseFromStick()
+    {
+        stuckTo = null;
+    }
+}
