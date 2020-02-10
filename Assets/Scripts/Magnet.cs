fdaba0444fcc1ec402b21b0a1ef582da9851db47
weijie016 <vege_wei_jie@hotmail.com>
2020-02-09 18:23:50 -0800

    magnet draw

--- Assets/Scripts/Magnet.cs
+++ Assets/Scripts/Magnet.cs
@@ -12,16 +12,22 @@ public class Magnet : MonoBehaviour
 	public MagneticPole pole = MagneticPole.N;
 	public float permeability = 0.05f;
 
+	private CapsuleCollider2D capsule;
+	private Transform trans;
+	private LineRenderer lr;
+
 	// Start is called before the first frame update
 	void Start()
 	{
-
+		capsule = this.GetComponent<CapsuleCollider2D>();
+		trans = this.transform;
+		lr = this.GetComponent<LineRenderer>();
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-
+		DrawCapsule();
 	}
 
 	private void OnTriggerStay2D(Collider2D collision)
@@ -30,7 +36,8 @@ public class Magnet : MonoBehaviour
 		if (magnetComp != null)
 		{
 			Vector3 magnetForce = CalculateGilbertForce(this, magnetComp);
-			this.transform.parent.GetComponent<Rigidbody2D>().AddForce(magnetForce);
+			this.transform.root.GetComponent<Rigidbody2D>().AddForce(magnetForce);
+			// this.transform.parent.GetComponent<Rigidbody2D>().AddForce(magnetForce);
 		}
 	}
 
@@ -57,5 +64,155 @@ public class Magnet : MonoBehaviour
 		}
 
 		return f * r.normalized;
+	}
+
+	/*
+	 * Draws the magnetic field
+	 */
+	void DrawCapsule()
+	{
+		float rotation = trans.rotation.eulerAngles.z;
+		print("rotation: " + rotation);
+		Vector2 colliderSize = trans.lossyScale * capsule.size;
+
+		// if one is smaller, emulate it to a circle collider
+		if (capsule.direction == CapsuleDirection2D.Horizontal && colliderSize.x < colliderSize.y)
+		{
+			colliderSize.x = colliderSize.y;
+		}
+		if (capsule.direction == CapsuleDirection2D.Vertical && colliderSize.x > colliderSize.y)
+		{
+			colliderSize.y = colliderSize.x;
+		}
+		Vector2 colliderCenter = trans.TransformPoint((Vector2)trans.localPosition + capsule.offset);
+
+		if (capsule.direction == CapsuleDirection2D.Horizontal)
+		{
+			float radius = colliderSize.y / 2;
+			float lengthToCurve = (colliderSize.x - colliderSize.y) / 2;
+			Vector2 topRight = RotateAroundPivot(new Vector2(colliderCenter.x + lengthToCurve,
+				colliderCenter.y + radius),
+				colliderCenter,
+				rotation);
+			Vector2 topLeft = RotateAroundPivot(new Vector2(colliderCenter.x - lengthToCurve,
+				colliderCenter.y + radius),
+				colliderCenter,
+				rotation);
+			Vector2 botRight = RotateAroundPivot(new Vector2(colliderCenter.x + lengthToCurve,
+				colliderCenter.y - radius),
+				colliderCenter,
+				rotation);
+			Vector2 botLeft = RotateAroundPivot(new Vector2(colliderCenter.x - lengthToCurve,
+				colliderCenter.y - radius),
+				colliderCenter,
+				rotation);
+			Vector3 rightCurveCenter = RotateAroundPivot(new Vector3(colliderCenter.x + lengthToCurve,
+				colliderCenter.y, 0),
+				colliderCenter,
+				rotation);
+			Vector3 leftCurveCenter = RotateAroundPivot(new Vector3(colliderCenter.x - lengthToCurve,
+				colliderCenter.y, 0),
+				colliderCenter,
+				rotation);
+			int segments = 180;
+			int pointCounts = 2 * segments + 5;
+			Vector3[] points = new Vector3[pointCounts];
+			points[0] = topLeft;
+			points[1] = topRight;
+
+			for (int i = 0; i < segments; i++)
+			{
+				float rad = Mathf.Deg2Rad * (i * 180f / segments);
+				points[i + 2] = RotateAroundPivot(new Vector3(Mathf.Sin(rad) * radius, Mathf.Cos(rad) * radius, 0) + rightCurveCenter,
+					rightCurveCenter,
+					rotation);
+			}
+
+			points[segments + 2] = botRight;
+			points[segments + 3] = botLeft;
+
+			for (int i = 0; i < segments; i++)
+			{
+				float rad = Mathf.Deg2Rad * (180f + i * 180f / segments);
+				points[i + segments + 4] = RotateAroundPivot(new Vector3(Mathf.Sin(rad) * radius, Mathf.Cos(rad) * radius, 0) + leftCurveCenter,
+					leftCurveCenter,
+					rotation);
+			}
+
+			points[2 * segments + 4] = topLeft;
+
+			lr.positionCount = pointCounts;
+			lr.SetPositions(points);
+		}
+		else if (capsule.direction == CapsuleDirection2D.Vertical)
+		{
+			float radius = colliderSize.x / 2;
+			float lengthToCurve = (colliderSize.y - colliderSize.x) / 2;
+			Vector2 topRight = RotateAroundPivot(new Vector2(colliderCenter.x + radius,
+				colliderCenter.y + lengthToCurve),
+				colliderCenter,
+				rotation);
+			Vector2 topLeft = RotateAroundPivot(new Vector2(colliderCenter.x - radius,
+				colliderCenter.y + lengthToCurve),
+				colliderCenter,
+				rotation);
+			Vector2 botRight = RotateAroundPivot(new Vector2(colliderCenter.x + radius,
+				colliderCenter.y - lengthToCurve),
+				colliderCenter,
+				rotation);
+			Vector2 botLeft = RotateAroundPivot(new Vector2(colliderCenter.x - radius,
+				colliderCenter.y - lengthToCurve),
+				colliderCenter,
+				rotation);
+			Vector3 topCurveCenter = RotateAroundPivot(new Vector3(colliderCenter.x,
+				colliderCenter.y + lengthToCurve, 0),
+				colliderCenter,
+				rotation);
+			Vector3 botCurveCenter = RotateAroundPivot(new Vector3(colliderCenter.x,
+				colliderCenter.y - lengthToCurve, 0),
+				colliderCenter,
+				rotation);
+			int segments = 180;
+			int pointCounts = 2 * segments + 5;
+			Vector3[] points = new Vector3[pointCounts];
+			points[0] = botLeft;
+			points[1] = topLeft;
+
+			for (int i = 0; i < segments; i++)
+			{
+				float rad = Mathf.Deg2Rad * (270f + i * 180f / segments);
+				points[i + 2] = RotateAroundPivot(new Vector3(Mathf.Sin(rad) * radius, Mathf.Cos(rad) * radius, 0) + topCurveCenter,
+					topCurveCenter,
+					rotation);
+			}
+
+			points[segments + 2] = topRight;
+			points[segments + 3] = botRight;
+
+			for (int i = 0; i < segments; i++)
+			{
+				float rad = Mathf.Deg2Rad * (90f + i * 180f / segments);
+				points[i + segments + 4] = RotateAroundPivot(new Vector3(Mathf.Sin(rad) * radius, Mathf.Cos(rad) * radius, 0) + botCurveCenter,
+					botCurveCenter,
+					rotation);
+			}
+
+			points[2 * segments + 4] = botLeft;
+
+			lr.positionCount = pointCounts;
+			lr.SetPositions(points);
+		}
+	}
+
+	public static Vector2 RotateAroundPivot(Vector2 aPoint, Vector2 aPivot, float aDegree)
+	{
+		aPoint -= aPivot;
+		float rad = aDegree * Mathf.Deg2Rad;
+		float s = Mathf.Sin(rad);
+		float c = Mathf.Cos(rad);
+		return aPivot + new Vector2(
+			aPoint.x * c - aPoint.y * s,
+			aPoint.y * c + aPoint.x * s
+		);
 	}
 }
