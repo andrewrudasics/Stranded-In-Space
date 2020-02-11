b3992a011c0098e5daba4689882a7ebf5a5bb03b
elliottdebruin <elliottdebruin@me.com>
2020-02-11 15:10:57 -0800

    astorid barrier done, still need to add prefab

--- Assets/Scripts/Player2D.cs
+++ Assets/Scripts/Player2D.cs
@@ -4,16 +4,47 @@ using UnityEngine;
 
 public class Player2D : MonoBehaviour
 {
+    SpriteRenderer spriteRenderer;
+    Sprite s1;
+    Sprite s2;
+    private bool dragging;
+    private float moveSpeed = (float)2.0; 
+    private Vector2 targetPos;
+    //public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        dragging = false;
+
+        targetPos = transform.position;
+        //rb = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        faceMouse();
+    	if (Input.GetKeyDown(KeyCode.Mouse0)) 
+         {
+             if (Input.GetMouseButtonDown(0)) 
+             {
+        		dragging = true;
+                //spriteRenderer.sprite = s2;
+        	}
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            dragging = false;
+            move();
+            //rb.velocity = 2.0f * Vector3.up;
+     
+            //spriteRenderer.sprite = s1; //sets sprite renderers sprite
+        }
+
+        if (dragging)
+        {
+            faceMouse();
+        }
     }
 
     void faceMouse()
@@ -26,6 +57,13 @@ public class Player2D : MonoBehaviour
     	);
 
     	transform.up = direction;
+    }
+
+    void move()
+    {
+        var targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //targetPos.z = transform.position.z;
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, moveSpeed);
     }
     
 }
