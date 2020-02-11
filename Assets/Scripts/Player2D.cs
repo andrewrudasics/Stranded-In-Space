bd1b3fd74823a79b38176a57e2c492b4415c84e6
elliottdebruin <elliottdebruin@me.com>
2020-02-10 20:57:57 -0800

    player following mouse

---
+++ Assets/Scripts/Player2D.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2D : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        faceMouse();
+    }
+
+    void faceMouse()
+    {
+    	Vector3 mousePosition = Input.mousePosition;
+    	mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+    	Vector2 direction = new Vector2(
+    		mousePosition.x - transform.position.x,
+    		mousePosition.y - transform.position.y
+    	);
+
+    	transform.up = direction;
+    }
+    
+}
