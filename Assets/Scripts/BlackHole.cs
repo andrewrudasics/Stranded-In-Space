fda29a5ad9de9bad329db46d1e3bc40f7fa5be8b
weijie016 <vege_wei_jie@hotmail.com>
2020-02-11 23:45:49 -0800

    crate prefabs

---
+++ Assets/Scripts/BlackHole.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHole : MonoBehaviour
+{
+	public float mass;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	//private void OnTriggerStay2D(Collider2D collision)
+	//{
+	//	collision.transform.position;
+	//}
+
+	///*
+	// * code from https://forum.unity.com/threads/vortex-black-hole-physics.297783/
+	// */
+	//public static Vector3 GAcceleration()
+	//{
+
+	//}
+}
