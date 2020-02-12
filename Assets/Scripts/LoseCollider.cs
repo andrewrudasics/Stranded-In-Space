d7bfff4a313bb34e4b193b9ee64143b7598e0d5a
elliottdebruin <elliottdebruin@me.com>
2020-02-12 13:10:02 -0800

    level barrier fix

--- Assets/Scripts/LoseCollider.cs
+++ Assets/Scripts/LoseCollider.cs
@@ -20,7 +20,7 @@ public class LoseCollider : MonoBehaviour
         
     }
 
-	private void OnTriggerEnter2D(Collider2D collision)
+	private void OnTriggerExit2D(Collider2D collision)
 	{
 		if (collision.gameObject.tag == "Player")
 		{
